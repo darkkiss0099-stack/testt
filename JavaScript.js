1be676d7521a8156f05b3ed91071da7ff600c390
drakegg <darkkiss0099@gmail.com>
2026-01-17 14:31:08 +0700

    JavaScript.js

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSy...",
+  authDomain: "your-project.firebaseapp.com",
+  databaseURL: "https://your-project-default-rtdb.firebaseio.com",
+  projectId: "your-project",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "...",
+  appId: "..."
+};
